fe94ab222ae6df5a562ccec717dcf6577c01dbd1
angelmungg <73095263+angelmungg@users.noreply.github.com>
2020-11-14 18:02:37 +0700

    Update ConfirmServlet.java

--- src/main/java/Servlet/ConfirmServlet.java
+++ src/main/java/Servlet/ConfirmServlet.java
@@ -6,8 +6,13 @@
 package Servlet;
 
 import Model.Cart;
+import Model.Cart.LineItem;
+import entity.Customer;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +21,7 @@ import javax.servlet.http.HttpSession;
 
 /**
  *
- * @author User
+ * @author paw
  */
 public class ConfirmServlet extends HttpServlet {
 
@@ -30,20 +35,36 @@ public class ConfirmServlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-           throws ServletException, IOException {
-        HttpSession session = request.getSession(false);
-        Cart cart = (Cart) session.getAttribute("cart");
-        if (session == null || cart == null) {
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Session not available");
-            return;
-        }     
-         
+             throws ServletException, IOException {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.mycompany_ProjectWebPro_war_1.0-SNAPSHOTPU");
+        EntityManager em = emf.createEntityManager();
+        String checkout = request.getParameter("purchase");
         
-        session.setAttribute("cart", cart);
-  
-        request.getRequestDispatcher("/Purchase.jsp").forward(request, response);
-    
+        HttpSession session = request.getSession();
+        HttpSession cart = request.getSession();
+        Cart c = (Cart) session.getAttribute("cart");
+        Customer customerid = (Customer) session.getAttribute("user");
+        Integer payment = (int) ((Math.random() * ((1000 - 1) + 1)) + 1);
+        Integer gen = (int) ((Math.random() * ((1000 - 1) + 1)) + 1);
+      
+        em.getTransaction().begin();
+        em.createNativeQuery("insert into Payment (paymentId,orderDetail) values "+"("+payment+",'"+customerid.getCustomerName()+"')").executeUpdate();
+        em.createNativeQuery("insert INTO dbwebpro.Order (orderId, Payment_paymentId, Customer_customerId ) VALUES "
+                + "("+gen+","+payment+","+customerid.getCustomerId()+")").executeUpdate();
+        for (LineItem line : c.getItems()) {
+             em.createNativeQuery("insert INTO Orderdetail (orderId,productId,orderQuantity,orderPrice) VALUES ("+gen+","+line.getProduct().getProductId()+","+line.getQuantity()+","+line.getPrice()+")").executeUpdate();
+        
+        }
+        
+        
+        session.removeAttribute("cart");
+        em.getTransaction().commit();
+        em.close();
+        
+        request.getRequestDispatcher("Thankyou.jsp").forward(request, response);
     }
+        
+    
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
